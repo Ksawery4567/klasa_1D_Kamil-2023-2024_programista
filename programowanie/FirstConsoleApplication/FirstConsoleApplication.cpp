0a13836f595141a1e1cf1f826855c0132d01872d
fil <nhhj>
2023-10-11 09:46:17 +0200

    OK

--- programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
+++ programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -2,10 +2,17 @@
 //
 
 #include <iostream>
-
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World\n";
+	std::cout << "My names is Kamil\n";
+	std::cout << 'X'
+	std::cout << "\n"
+	std::cout << '\n'  
+	std::cout << 257 << "\n" //sta³a liczbowa ca³kowita
+	std::cout << 12.5 << "\n" //sta³a liczbowa rzeczywista
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -18,4 +25,3 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
- 
