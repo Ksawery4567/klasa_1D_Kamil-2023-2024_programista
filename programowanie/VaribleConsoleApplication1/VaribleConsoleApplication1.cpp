71ae6702c8b921599cfcf0bc351899e0b1c50058
fil <nhhj>
2023-10-25 09:54:24 +0200

    koniec lekcji

---
+++ programowanie/VaribleConsoleApplication1/VaribleConsoleApplication1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+using namespace std;
+//Napisz program, który wczyta od u¿ytkownika jego wiek a nastêpnie wyœwietli go na konsli
+int main()
+{
+    short ageFromUser;
+    std::cout << "Podaj swoj wiek.";
+    std::cin >> ageFromUser;
+
+
+    std::cout << "Masz" << ageFromUser << "lat.";
+//Stwórz program, który pobiera od u¿ytkowanika
+//d³ugoœæ w metrach i przelicza j¹ na centymetry,
+//milimetry i kilometry. Wyœwietl wynik.
+void task2()
+{
+    float lenghtInMeterFromUser;
+    std::cout << "Podaj d³ugoœæ w metrach";
+    std::cin >> lenghtInMeterFromUser;
+    float lenghtInMeterFromUser;
+    lenghtInMeterFromUser = lenghtInMeterFromUser /100
+}
+
+int maim()
+{
+    //taskl();
+     task2();
+}
+
+/*
+
+Algorytm - zbiór instrukcji realizuj¹cy postawione przed nim zadanie.
+    Sposoby zapisu:
+      * w punktach
+      * opis s³owny 
+      * jêzyk programowania lub pseudokod
+      * zbiór rysunków
+      * schemat blokowy
+Zmienna to obszar pamiêci, s³u¿¹cy do przechowywania danych. Te dane mo¿na modyfikowaæ.
+Zmienna pozwala przechowywaæ tylko jedn¹ wartoœæ w danym momencie czasu.
+
+Deklaracja zmiennej - moment jej utworzenia.
+
+typ_zmiennej nazwa_zmiennej;
+
+Typ zmiennej - mówi o wielkoœci obszarów w pamiêci i o rodzaju przechowaywaniu danych.
+
+short       Liczby ca³kowite ze znakiem   2 bajty <-32 768; 32 767>
+int/long    Liczby ca³kowite ze znakiem   4 bajty <-2 147 483 648; 2 147 483 647>
+long long   Liczby ca³kowite ze znakiem   8 bajtów <-9 223 372 036 854 775 808, 9 223 372 036 854 775 807>
+
+Je¿eli przed podwy¿szymi typami dodany s³owo "unsigned" to zakres
+jest od zera do podwójnego zakresu górnego plus 1.
+
+float        liczby rzeczywiste ze znakiem  4 bajty
+double       liczby rzeczywiste ze znakiem  8 bajtów 
+long double  liczby rzeczywiste ze znakiem  12 bajtów
+
+Nazwa zmiennej - nazwa obszaru w pamiêci. Poprzez t¹ nazwê odwo³ujemy siê do tego obszaru.
+
+Wymagania kompolatora wzglêdem nazwy:
+* u¿ycie tylko dozwolonych znaków 
+- alfoabet angielski a-z A-Z
+-cyfry arabskie 0-9
+-podkreœlenie (pod³oga) _
+* pierwszym znakiem nie mo¿e byæ cyfra
+* nie mo¿e byæ s³owo kluczwe danego jêzyka programowania
+* musi byæ unikalna w danej widocznoœci
+
+Wymagania programistów:
+* je¿eli nazwa sk³ada siê z wielu wyrazów to zamiast spacji:
+ -u¿ywaj znaków podkreœlenia np. first_number_from_user
+ -stosujemy camel case, czyli piszemy wszystko razem a s³owo (zaczynamy od drugiego)
+  zaczynamy z du¿ej litery np. firstNumberFromUser
+* nazwa zmiennej musi oddawaæ charakter przechowywanych danych, czyli mieæ znaczenie
+* nazwy po angielsku !!!!      
+
+
+
+
+
+*/
